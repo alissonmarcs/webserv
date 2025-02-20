625edbc8c428b48a62656d4fdc95044aee063e13
Alisson Marcos <alissonmarcos249@gmail.com>
2025-02-20 11:40:55 -0300

    wip

--- cgi-bin/grite.sh
+++ cgi-bin/grite.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-printf "HTTP/1.1 200 OK\r\n"
-printf "Content-Type: text/html\r\n"
-printf "\r\n"
-
 OUTPUT=$(tr a-z A-Z)
-printf "$OUTPUT\n"
+LEN=${#OUTPUT}
+
+function print_status ()
+{
+    printf "HTTP/1.1 %d %s\r\n" "$1" "$2"
+    printf "Content-Type: text/html\r\n"
+    printf "\r\n"
+}
+
+if [ $LEN == 0 ]; then
+    print_status 200 "OK"
+    printf "<h1>Please provide some text</h1>"
+    exit 0
+fi
+
+print_status 200 "OK"
+printf "<p>$OUTPUT</p>"
+exit 0
