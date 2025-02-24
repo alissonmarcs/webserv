ef72adb5da45cf94203fd0c4af5b0d464655ed25
Alisson <alissonmarcos249@gmail.com>
2025-02-24 13:11:39 -0300

    organizing functions in CGI

--- src/classes/Client/CGI.cpp
+++ src/classes/Client/CGI.cpp
@@ -1,5 +1,10 @@
 #include "Client.hpp"
 
+bool validateExtension (const string & file_name, const string & cgi_ext);
+int wait_child (pid_t pid, time_t child_born);
+string get_folder (const string & script_path);
+string get_script_name (string script_path);
+
 bool
 Client::isCGI()
 {
@@ -8,15 +13,19 @@ Client::isCGI()
   return (true);
 }
 
-bool validateExtension (const string & file_name, const string & cgi_ext)
+void
+Client::handleCGI ()
 {
-  size_t dot = file_name.rfind('.');
-  string ext;
-
-  if  (dot == string::npos)
-    return (false);
-  ext = file_name.substr(dot);
-  return (ext == cgi_ext);
+  if (!findScriptPath())
+    return ;
+  child_born = time (NULL);
+  pid = fork ();
+  if (pid == -1)
+    { setError(INTERNAL_SERVER_ERROR); return ; }
+  else if (pid == 0)
+    child ();
+  else
+    parent ();
 }
 
 bool
@@ -47,6 +56,62 @@ Client::findScriptPath()
   else if (access(script_path.c_str(), X_OK) == -1)
     { setError(FORBIDDEN); return (false); }
   return (true);
+}
+
+void
+Client::parent ()
+{
+  int status;
+  stringstream content;
+  
+  status = wait_child (pid, child_born); 
+  if (!checkChildStatus (status))
+    return ;
+  
+  ifstream file(CGI_FILE_OUT, ios::binary);
+  if (file.is_open() == false)
+    { setError(INTERNAL_SERVER_ERROR); return ; }
+   content << file.rdbuf();
+   response += content.str();
+  if (response.size() == 0)
+    { setError(BAD_GATEWAY); return ; }
+  file.close();
+  response_is_done = true;
+}
+
+void
+Client::child ()
+{
+ if (close (client_fd) < 0) 
+    FATAL_ERROR("close");
+
+  string folder = get_folder (script_path);
+  string script_name = get_script_name (script_path);
+  char *argv[] = {const_cast<char*>(script_name.c_str()), NULL};
+  
+  redirectStdin();
+  redirectStdout();
+  if (chdir(folder.c_str()) == -1)
+    FATAL_ERROR("chdir");
+  if (execve(script_name.c_str(), argv, NULL) == -1)
+    FATAL_ERROR("execve");
+}
+
+string get_folder (const string & script_path)
+{
+  size_t last_slash = script_path.rfind('/');
+  if (last_slash == string::npos)
+    return ("");
+  return (script_path.substr(0, last_slash + 1));
+}
+
+string get_script_name (string script_path)
+{
+  size_t last_slash = script_path.rfind('/');
+
+  if (last_slash == string::npos)
+    return ("");
+  return (script_path.substr(last_slash + 1));
 }
 
 void
@@ -83,39 +148,15 @@ Client::redirectStdout ()
     FATAL_ERROR("close");
 }
 
-string get_folder (const string & script_path)
+bool validateExtension (const string & file_name, const string & cgi_ext)
 {
-  size_t last_slash = script_path.rfind('/');
-  if (last_slash == string::npos)
-    return ("");
-  return (script_path.substr(0, last_slash + 1));
-}
+  size_t dot = file_name.rfind('.');
+  string ext;
 
-string get_script_name (string script_path)
-{
-  size_t last_slash = script_path.rfind('/');
-
-  if (last_slash == string::npos)
-    return ("");
-  return (script_path.substr(last_slash + 1));
-}
-
-void
-Client::child ()
-{
- if (close (client_fd) < 0) 
-    FATAL_ERROR("close");
-
-  string folder = get_folder (script_path);
-  string script_name = get_script_name (script_path);
-  char *argv[] = {const_cast<char*>(script_name.c_str()), NULL};
-  
-  redirectStdin();
-  redirectStdout();
-  if (chdir(folder.c_str()) == -1)
-    FATAL_ERROR("chdir");
-  if (execve(script_name.c_str(), argv, NULL) == -1)
-    FATAL_ERROR("execve");
+  if  (dot == string::npos)
+    return (false);
+  ext = file_name.substr(dot);
+  return (ext == cgi_ext);
 }
 
 int
@@ -138,27 +179,6 @@ wait_child (pid_t pid, time_t child_born)
   return (child_status);
 }
 
-void
-Client::parent ()
-{
-  int status;
-  stringstream content;
-  
-  status = wait_child (pid, child_born); 
-  if (!checkChildStatus (status))
-    return ;
-  
-  ifstream file(CGI_FILE_OUT, ios::binary);
-  if (file.is_open() == false)
-    { setError(INTERNAL_SERVER_ERROR); return ; }
-   content << file.rdbuf();
-   response += content.str();
-  if (response.size() == 0)
-    { setError(BAD_GATEWAY); return ; }
-  file.close();
-  response_is_done = true;
-}
-
 bool
 Client::checkChildStatus (int status)
 {
@@ -175,19 +195,4 @@ Client::getHeader (string key)
   if (request_headers.count (key) == 0)
     return "";
   return request_headers[key];
-}
-
-void
-Client::handleCGI ()
-{
-  if (!findScriptPath())
-    return ;
-  child_born = time (NULL);
-  pid = fork ();
-  if (pid == -1)
-    { setError(INTERNAL_SERVER_ERROR); return ; }
-  else if (pid == 0)
-    child ();
-  else
-    parent ();
 }
