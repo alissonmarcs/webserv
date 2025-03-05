b8b5d07a13bedf08c1f83d2ad1178afa34062a61
Alisson <alissonmarcos249@gmail.com>
2025-03-05 14:09:55 -0300

    remove some unnued cgi scripts

--- cgi-bin/image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf 'HTTP/1.1 200 OK\r\n'
-printf 'Content-Type: image/jpeg\r\n'
-printf '\r\n'
-# paste the full path to any image below
-cat /home/alisson/Pictures/codin-memes/height-calculator.png
-
-
--- cgi-bin/slow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Slowly read and write back the request
-
-printf "HTTP/1.1 200 OK\r\n"
-printf "Content-Type: text/plaint\r\n"
-printf "\r\n"
-
-env | while read line; do echo $line; sleep 1; done
