c778c4745149ba38ec32c9437453c6e4b1db6da3
Alisson <alissonmarcos249@gmail.com>
2025-03-05 19:34:06 -0300

    wip

---
+++ cgi-bin/pwd.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function print_status ()
+{
+    printf "HTTP/1.1 %d %s\r\n" "$1" "$2"
+    printf "Content-Type: text/html\r\n"
+    printf "\r\n"
+}
+
+current_folder=$(pwd)
+
+print_status 200 "OK"
+
+printf "<h1>Folder from where the script was executed: <code>'$current_folder'</code></h1>"
