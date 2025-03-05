29daa59286ac6952211d2ec921ae85f94463c1de
Alisson <alissonmarcos249@gmail.com>
2025-03-05 13:59:05 -0300

    cgi script and form to generate password

---
+++ cgi-bin/generate_password.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+import cgi
+import random
+import string
+
+def print_headers(status_code, phase, content_type):
+    print ("HTTP/1.1 %s %s" % (status_code, phase), end='\r\n')
+    print ("Content-type: %s" % content_type, end='\r\n')
+    print ("", end='\r\n')
+
+def generate_password(length, upper_case, numbers, symbols):
+    caracteres = string.ascii_lowercase  
+    if upper_case:
+        caracteres += string.ascii_uppercase
+    if numbers:
+        caracteres += string.digits
+    if symbols:
+        caracteres += string.punctuation
+    
+    senha = ''.join(random.choice(caracteres) for _ in range(length))
+    return senha
+
+form = cgi.FieldStorage()
+
+length = int(form.getvalue('length', 8))  # Valor padrão é 8
+upper_case = form.getvalue('uppercase') == '1'
+numbers = form.getvalue('numbers') == '1'
+symbols = form.getvalue('symbols') == '1'
+
+password = generate_password(length, upper_case, numbers, symbols)
+
+print_headers ("200", "OK", "text/html")
+
+print(f"<h1>Your password was generated!</h1>")
+print(f"<p style='font-size: 24px; font-weight: bold;'>{password}</p>")
