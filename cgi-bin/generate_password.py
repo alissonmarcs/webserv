feb3260e4e8a5ec5616b450ec76aa452242f2e7c
Alisson <alissonmarcos249@gmail.com>
2025-03-06 15:30:41 -0300

    upcase option on form of passwords

--- cgi-bin/generate_password.py
+++ cgi-bin/generate_password.py
@@ -12,15 +12,18 @@ def print_headers(status_code, phase, content_type):
     print ("Content-type: %s" % content_type, end='\r\n')
     print ("", end='\r\n')
 
-def generate_password(length, upper_case, numbers, symbols):
-    caracteres = string.ascii_lowercase  
+def generate_password(length, upper_case, lower_case, numbers, symbols):
+    caracteres = ""
+
     if upper_case:
         caracteres += string.ascii_uppercase
     if numbers:
         caracteres += string.digits
     if symbols:
         caracteres += string.punctuation
-    
+    if lower_case:
+        caracteres += string.ascii_lowercase
+
     senha = ''.join(random.choice(caracteres) for _ in range(length))
     return senha
 
@@ -28,10 +31,16 @@ form = cgi.FieldStorage()
 
 length = int(form.getvalue('length', 8))  # Valor padrão é 8
 upper_case = form.getvalue('uppercase') == '1'
+lower_case = form.getvalue('lowercase') == '1'
 numbers = form.getvalue('numbers') == '1'
 symbols = form.getvalue('symbols') == '1'
 
-password = generate_password(length, upper_case, numbers, symbols)
+if (not upper_case and not lower_case and not numbers and not symbols):
+    print_headers ("200", "OK", "text/html")
+    print ("<h1>At least one option must be selected!</h1>")
+    exit ()
+
+password = generate_password(length, upper_case, lower_case, numbers, symbols)
 
 print_headers ("200", "OK", "text/html")
 
